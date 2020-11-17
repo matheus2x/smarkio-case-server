464e9ba8849f8988385f33b8f7aa84eb226a112a
matheus2x <matheushenriqueplus99@gmail.com>
2020-11-17 18:19:43 -0300

    feat: add text to speech route

---
+++ src/app/controllers/commentsController.ts
@@ -0,0 +1,57 @@
+import { createWriteStream } from "fs";
+import { resolve } from "path";
+import { Request, Response } from "express";
+import { mysql, tts } from "../../config";
+
+const commentsController = {
+  async store(req: Request, res: Response) {
+    try {
+      const { comment } = req.body;
+
+      if (comment.length <= 0 || comment.length > 4000) {
+        return res.status(400).json({ Error: "Invalid Comment." });
+      }
+
+      const [commentId] = await mysql
+        .connection("comments")
+        .insert({ comment, audio: ".." });
+
+      const synthesizeParams = {
+        text: comment,
+        accept: "audio/mp3",
+        voice: "en-US_AllisonV3Voice",
+      };
+
+      const synthesizeStream = await tts.tts.synthesizeUsingWebSocket(
+        synthesizeParams
+      );
+
+      const speechFilePath = resolve(__dirname, "..", "..", "..", "uploads");
+      const speechFile = `speech-${commentId}.mp3`;
+
+      await synthesizeStream.pipe(
+        createWriteStream(`${speechFilePath}/${speechFile}`)
+      );
+
+      await mysql
+        .connection("comments")
+        .where("id", commentId)
+        .update({ audio: speechFile });
+
+      return res.json({
+        id: commentId,
+        comment,
+        audio: speechFile,
+      });
+    } catch (err) {
+      console.error(
+        "[Server Error]: Error while inserting new comment:\n",
+        err.message
+      );
+
+      return res.status(500).json({ Error: "Internal Server Error" });
+    }
+  },
+};
+
+export default commentsController;
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import commentsController from "./controllers/commentsController";
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
   return res.json({ Message: "Hello World" });
 });
+
+routes.post("/tts", commentsController.store);
 
 export default routes;
